50a0beabf0c29cb3788e6f236f3c6ea0fee17c76
Leonardo Geremia <leonardogeremiasc@gmail.com>
2021-11-25 21:20:31 -0300

    Creating photos atributes

--- src/controllers/photos/CreatePhotosController.ts
+++ src/controllers/photos/CreatePhotosController.ts
@@ -3,7 +3,14 @@ import { Request, Response } from "express";
 
 class CreatePhotosController {
     async handle(req: Request, res: Response) {
-        const { name, description, date } = req.body;
+        const { name, description, date, url } = req.body;
+        const photo = await {
+            "name": name,
+            "description": description,
+            "date": date,
+            "url": url
+        }
+        return res.json();
     }
 }
 
